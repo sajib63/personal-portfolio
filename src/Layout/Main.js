49b09a865231d74ac9254d9529b44ce8a0466284
Sajib Ahmed <sajib7315@gmail.com>
2022-12-10 14:11:04 +0600

    adding code

---
+++ src/Layout/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+
+
+
+const Main = () => {
+    return (
+        <div className='container mx-auto'>
+           <Navbar></Navbar>
+            
+        </div>
+    );
+};
+
+export default Main;
